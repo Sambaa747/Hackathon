fd4baba4834965415fff498ffba135af4425ef1a
Sambalhundev Soyolerdene <b18fa1412@ufe.edu.mn>
2022-04-23 19:17:42 +0800

    Deleted uncessesary files

---
+++ pages/buteegdehuun.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../layouts/Main'
+import Footer from '../components/footer'
+const Buteegdehuun = () => {
+  return (
+    <Layout>
+      <div>B</div>
+      <Footer />
+    </Layout>
+  )
+}
+
+export default Buteegdehuun
