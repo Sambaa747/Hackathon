19c68042c9630e6e304e08905d3039e2b352194d
nt-Munkhdelger <b18fa1417@ufe.edu.mn>
2022-04-24 14:35:03 +0800

    suulih zda

--- components/page-intro/index.js
+++ components/page-intro/index.js
@@ -1,50 +1,60 @@
 import SwiperCore, {EffectFade, Navigation} from 'swiper';
-import { useState, useRef } from 'react';
-import DisplayData from '../display-data/index'
-
-
-
-
+import { useState, useRef, useEffect } from 'react';
+import DisplayData from '../display-data/index';
+import axios from 'axios';
 import useOnClickOutside from 'use-onclickoutside';
 
 
 SwiperCore.use([EffectFade, Navigation]);
 
+
+
 const PageIntro = () => {
-    
- 
+  useEffect(() => {
+    axios.get('http://192.168.1.181:8000/api/Macbook')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
+  // const { allData, error } = useSWR(`http://192.168.1.181:8000/api/${form}`, fetcher)
   const [searchOpen, setSearchOpen] = useState(false);
-  const [todos, setTodos] = useState([]);
-  const [form, setForm] = useState({
-    title: "",
-    id: "",
-  });
+  const [data, setData] = useState([])
+  const [form, setForm] = useState("");
 
-  async function todoData(e) {
-    e.preventDefault();
-    if(form.search == ""){
-      alert("Хайх гэж буюу нэр оруулна уу?")
-    } else {
-      const data =  await fetch(
-        `https://jsonplaceholder.typicode.com/todos?q=${form.search}`
-      )
-      .then((res) => console.log(res.json()))
-      .then((data) => data)
+  function getProduct(e){
+      e.preventDefault();
 
-      setTodos({data: data});
-     
-    }
   }
 
-  const searchChangeHandler = (e) => {
+  const fetchData = async () => {
+    const req = await fetch('http://192.168.1.181:8000/api/Macbook', {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+      },
+    });
+    const newData = await req.json();
+    console.log(newData.results)
+    return setData(newData.results);
+
+};
+   const handleChange = (e) => {
     let name = e.target.name
     let value = e.target.value
 
-    if(name == "id") {
-      setForm({ ...form, id: value })
-    }
+    // if(name == "name") {
+    //    setForm({ ...form, name: value })
+    // }
     console.log(value)
+    setForm(value)
+    
   }
+
+  if (!data) return <p>No data</p>
+
+
 
   const searchRef = useRef(null);
 
@@ -53,7 +63,6 @@ const PageIntro = () => {
   }
   useOnClickOutside(searchRef, closeSearch);
   
-
   return (
     <section className="page-intro">  
           <div className="page-intro__slide" style={{ background: "url('images/banner/gradient.gif')" }}>
@@ -62,14 +71,14 @@ const PageIntro = () => {
                 <h3 >Худалдан авах бараа бүтээгдэхүүнийхээ үнийн харьцуулалтыг нэг дороос хийх боломж ...</h3>
                 <button ref={searchRef} className={`search-form-wrapper ${searchOpen ? 'search-form--active' : ''}`}>
                 <a href="#" className="btn-shop"><i onClick={() => setSearchOpen(!searchOpen)}  className="icon-search"></i></a>
-                  <form className={`search-form`} onSubmit={(e) => todoData(e)}>
+                  <form className={`search-form`} onSubmit={getProduct}>
                       <i className="icon-cancel" onClick={() => setSearchOpen(!searchOpen)}></i>
                       <input 
                       type="text" 
                       name="search" 
                       size="80" 
                       placeholder="Хайх гэж буй бүтээгдэхүүнээ бичнэ үү"
-                      onChange={searchChangeHandler} />
+                      onChange={(e) => handleChange(e)} />
                   </form>  
           </button>
               </div>
@@ -87,9 +96,9 @@ const PageIntro = () => {
         </div>
 
       {/* {console.log(weather)} */}
-      {todos.data != undefined ? (
+      {data.data != undefined ? (
         <div>
-          <DisplayData data={todos.data} />
+          <DisplayData data={data.data} />
         </div>
       ) : null}   
       </div>
